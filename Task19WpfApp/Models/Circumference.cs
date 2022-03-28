7a59da24debe2f4a91eb7e2b9edceb7c1d52180b
Кирилл Аксенов <kirill.ax@tprofeng.ru>
2022-03-28 09:30:24 +0300

    Добавьте файлы проекта.

---
+++ Task19WpfApp/Models/Circumference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task19WpfApp.Models
+{
+    class Circumference
+    {
+        public static double Radius(double r) => 2 * r * Math.PI;
+    }
+}
